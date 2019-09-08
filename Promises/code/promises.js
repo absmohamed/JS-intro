a885327be3accde45fce11eaf52b00dddd4dd940
janelbrandon <janel.brandon@coderacademy.edu.au>
2019-09-08 19:42:32 +1000

    added content for promises lesson

---
+++ Promises/code/promises.js
@@ -0,0 +1,38 @@
+// Using promises
+
+function asyncGetJoke() {
+  return new Promise(function (resolve, reject) {
+    $.getJSON("https://icanhazdadjoke.com/", (dadJoke) => {
+      if (!dadJoke) resolve(dadJoke.joke);
+      else reject("Error getting joke");
+    });
+  });
+}
+
+function compileJokes(num, callback) {
+  let jokes = [];
+  for (let i = 0; i <= num; i++) {
+    asyncGetJoke()
+      .then((dadJoke) => {
+        jokes.push(dadJoke);
+        if (i == 5) callback(jokes);
+      });
+  }
+}
+
+function logAndDisplayJokes(jokes) {
+
+  let jokesDiv = document.createElement("div");
+  let body = document.querySelector("body");
+  body.appendChild(jokesDiv);
+  for (let joke of jokes) {
+    let jokePara = document.createElement("p");
+    jokePara.innerText = joke;
+    jokesDiv.appendChild(jokePara);
+  }
+  console.log(jokes);
+}
+
+document.querySelector("#button").addEventListener("click", () => {
+  compileJokes(5, logAndDisplayJokes);
+});
